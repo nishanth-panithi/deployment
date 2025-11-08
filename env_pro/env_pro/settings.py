214b8bff406e3012e53b50089c90b8265cb229ad
nishanth-panithi <nishanthpanithi@gmail.com>
2025-11-08 20:08:31 +0530

    next

--- env_pro/env_pro/settings.py
+++ env_pro/env_pro/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'env_app'
+    'env_app',
 ]
 
 MIDDLEWARE = [
--- env_pro/env_pro/wsgi.py
+++ env_pro/env_pro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'env_pro.env_pro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'env_pro.settings')
 
 application = get_wsgi_application()
